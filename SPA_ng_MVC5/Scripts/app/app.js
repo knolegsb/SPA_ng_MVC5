536a57b26f55f4aac9aa24e24db660edc7043c27
knolegsb <seon2you@hotmail.com>
2015-12-21 00:23:56 -0800

    ngroute

--- SPA_ng_MVC5/Scripts/app/app.js
+++ SPA_ng_MVC5/Scripts/app/app.js
@@ -1,3 +1,11 @@
 ﻿/// <reference path="../angular.js" />
 
-var module = angular.module('StudentApp', ['studentController', 'studentResource']);
+var module = angular.module('StudentApp', ['ngRoute','studentController', 'studentResource']);
+
+module.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/home', {
+            templateUrl: 'templates/home.html',
+            controller: 'GetStudentList'
+        });
+}]);
